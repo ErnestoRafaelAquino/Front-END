3522f916058db3f87695966d248b472600efc074
ErnestoRafaelAquino <ernestorafaelaquino@outlook.es>
2022-08-08 17:42:26 -0300

    Agregado_seccion_2
    formación y proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderNavComponent } from './componentes/header-nav/header-nav.component';
 import { AsideBanerComponent } from './componentes/aside-baner/aside-baner.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderNavComponent,
-    AsideBanerComponent
+    AsideBanerComponent,
+    FormacionComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
